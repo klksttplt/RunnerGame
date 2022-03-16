727d476e5563ee8dc624cf7c1b4a4923cba35b10
klksttplt <63853743+klksttplt@users.noreply.github.com>
2022-03-16 12:35:52 +0200

    Speed blocks

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,29 +3,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float acceleration = 2.5f;
+    [Header("Acceleration")] public float acceleration = 2.5f;
+    public float deacceleration = 5f;
+
+    [Header("Movement speed")] public float startSpeed = 2f;
     public float movementSpeed = 4f;
-    public float jumpingSpeed = 6f;
+    public float movementSpeedRight = 8f;
+    public float movementSpeedLeft = 2f;
+
+    [Header("Jumping")] public float normalJumpingSpeed = 6f;
+    public float longJumpingSpeed = 10f;
     public float jumpDuration = 0.75f;
     public float verticalWallJumpingSpeed = 5f;
     public float horizontalWallJumpingSpeed = 3.5f;
-    public float startSpeed = 2f;
+
 
     public Action onCollectCoin;
 
+    private int _jumpCount = 0;
+
     private float _speed;
     private float _jumpingTimer = 0f;
+    private float _jumpingSpeed;
 
     private bool _canJump = false;
     private bool _jumping = false;
     private bool _canWallJump = false;
     private bool _wallJumpLeft = false;
     private bool _pause = false;
+    private bool _onSpeedAreaLeft = false;
+    private bool _onSpeedAreaRight = false;
+    private bool _onLongJump = false;
 
     // Start is called before the first frame update
     private void Start()
     {
         _speed = startSpeed;
+        _jumpingSpeed = normalJumpingSpeed;
     }
 
     // Update is called once per frame
@@ -33,7 +47,14 @@ public class Player : MonoBehaviour
     {
         //Accelerate the player 
         _speed += acceleration * Time.deltaTime;
-        if (_speed > movementSpeed) _speed = movementSpeed;
+
+        var targetSpeed = movementSpeed;
+        if (_onSpeedAreaLeft)
+            targetSpeed = movementSpeedLeft;
+
+        else if (_onSpeedAreaRight) targetSpeed = movementSpeedRight;
+
+        if (_speed > targetSpeed) _speed -= deacceleration * Time.deltaTime;
 
         //Move horizontally
         GetComponent<Rigidbody>().velocity = new Vector3(
@@ -43,9 +64,10 @@ public class Player : MonoBehaviour
         );
 
         //Check for input
-        var pressingJumpButton = Input.GetMouseButton(0) || Input.GetKey("space");
+        var pressingJumpButton = Input.GetMouseButtonDown(0) || Input.GetKeyDown("space");
+
         if (pressingJumpButton)
-            if (_canJump)
+            if ( _canJump)
                 _jumping = true;
 
         //Checking if player is paused
@@ -54,14 +76,23 @@ public class Player : MonoBehaviour
         //Make the player jump
         if (_jumping)
         {
+            
             _jumpingTimer += Time.deltaTime;
 
             if (pressingJumpButton && _jumpingTimer < jumpDuration)
+            {
+                if (_onLongJump)
+                {
+                    _jumpingSpeed = longJumpingSpeed;
+                }
+
                 GetComponent<Rigidbody>().velocity = new Vector3(
                     GetComponent<Rigidbody>().velocity.x,
-                    jumpingSpeed,
+                    _jumpingSpeed,
                     GetComponent<Rigidbody>().velocity.z
                 );
+                
+            }
         }
 
         //Make the player wall jump
@@ -96,6 +127,15 @@ public class Player : MonoBehaviour
             Destroy(otherCollider.gameObject);
             onCollectCoin();
         }
+
+        if (otherCollider.GetComponent<SpeedArea>() != null)
+        {
+            var speedArea = otherCollider.GetComponent<SpeedArea>();
+            if (speedArea.direction == Direction.Left) _onSpeedAreaLeft = true;
+            else if (speedArea.direction == Direction.Right) _onSpeedAreaRight = true;
+        }
+
+        if (otherCollider.GetComponent<LongJumpBlock>() != null) _onLongJump = true;
     }
 
     private void OnTriggerStay(Collider otherCollider)
@@ -103,9 +143,13 @@ public class Player : MonoBehaviour
         //Floor collider
         if (otherCollider.CompareTag("JumpingArea"))
         {
+            
+            
             _canJump = true;
             _jumping = false;
+            _jumpingSpeed = normalJumpingSpeed;
             _jumpingTimer = 0;
+            
         }
         //Wall collider
         else if (otherCollider.CompareTag("WallJumpingArea"))
@@ -113,11 +157,23 @@ public class Player : MonoBehaviour
             _canWallJump = true;
             _wallJumpLeft = transform.position.x < otherCollider.transform.position.x;
         }
+        
+        if (otherCollider.GetComponent<LongJumpBlock>() != null) _jumpingSpeed = longJumpingSpeed;
+        
     }
 
     private void OnTriggerExit(Collider otherCollider)
     {
         //Wall collider
         if (otherCollider.CompareTag("WallJumpingArea")) _canWallJump = false;
+
+        if (otherCollider.GetComponent<SpeedArea>() != null)
+        {
+            var speedArea = otherCollider.GetComponent<SpeedArea>();
+            if (speedArea.direction == Direction.Left) _onSpeedAreaLeft = false;
+            else if (speedArea.direction == Direction.Right) _onSpeedAreaRight = false;
+        }
+
+        if (otherCollider.GetComponent<LongJumpBlock>() != null) _onLongJump = false;
     }
 }
---
+++ Assets/Scripts/SpeedArea.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction
+{
+    Left,
+    Right
+}
+
+public class SpeedArea : MonoBehaviour
+{
+
+    public Direction direction;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
